b0dddf7f936875df7c59089f92b57dc1e2df83bb
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-04-17 20:50:13 +0900

    [회원] 회원가입 충돌 부분 해결

--- src/main/java/com/firstpj/member/controller/MemberController.java
+++ src/main/java/com/firstpj/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.firstpj.member.controller;
 
+import com.firstpj.member.model.MemberSignUp;
 import com.firstpj.member.service.impl.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 /**
  * packageName    : com.firstpj.api.member.controller
@@ -32,5 +36,17 @@ public class MemberController {
     public String deletePostByPathId(@PathVariable String id){
         memberService.deleteById(id);
         return "해당 글이 삭제 되었습니다.";
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody MemberSignUp memberSignUp){
+        boolean isSuccess = memberService.signup(memberSignUp);
+
+        if(isSuccess){
+            return ResponseEntity.ok(Collections.singletonMap("message","회원가입이 완료되었습니다."));
+
+        }else {
+            return ResponseEntity.badRequest().body(Collections.singletonMap("message","회원가입에 실패하였습니다."));
+        }
     }
 }
--- src/main/java/com/firstpj/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/firstpj/member/service/impl/MemberServiceImpl.java
@@ -1,10 +1,14 @@
 package com.firstpj.member.service.impl;
 
+import com.firstpj.jpa.entity.MemberEntity;
 import com.firstpj.jpa.repository.MemberRepository;
+import com.firstpj.member.model.MemberSignUp;
 import com.firstpj.member.service.MemberService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -25,10 +29,37 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @CacheEvict(value = "comments",allEntries = true)
     public void deleteById(String id) {
         Integer idInt=Integer.parseInt(id);
         memberRepository.deleteById(idInt);
+    }
+
+    @Override
+    public boolean signup(MemberSignUp memberSignUp) {
+        String email = memberSignUp.getEmail();
+        String password = memberSignUp.getPassword();
+        String author = memberSignUp.getAuthor();
+
+        // 유효성 검사
+        if( email == null || email.isEmpty()){
+            return false; // 이름이 없으면 회원가입 실패
+        }
+
+        //비밀번호 암호화
+        String EncoderPassword = passwordEncoder.encode(password);
+
+        // 유저가 있으면 ID만 등록 아니면 유저도 만들기
+        MemberEntity member = memberRepository.findByEmail(email).orElseGet(() ->
+                memberRepository.save(MemberEntity.builder()
+                        .email(email)
+                        .password(EncoderPassword)
+                        .author(author)
+                        .build())
+        );
+        return true;
     }
 
 }
