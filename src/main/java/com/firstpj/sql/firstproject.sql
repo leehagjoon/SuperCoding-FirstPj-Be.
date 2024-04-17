dd1a31b677c8b524faa92a4080ea17743450f7f5
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-04-17 13:33:51 +0900

    [SQL] create문 수정

--- src/main/java/com/firstpj/sql/firstproject.sql
+++ src/main/java/com/firstpj/sql/firstproject.sql
@@ -1,51 +1,31 @@
-CREATE TABLE `MEMBER` (
-	`member_id`	INT	NOT NULL,
-	`email`	VARCHAR(25)	NOT NULL,
-	`password`	VARCHAR(50)	NOT NULL,
-	`author`	VARCHAR(10)	NOT NULL
-);
-
-CREATE TABLE `POST` (
-	`post_id`	INT	NOT NULL,
-	`member_id`	INT	NOT NULL,
-	`title`	VARCHAR(50)	NOT NULL,
-	`content`	TEXT	NOT NULL,
-	`author`	VARCHAR(10)	NOT NULL,
-	`create_at`	DATETIME(6)	NOT NULL
-);
-
-CREATE TABLE `COMMENTS` (
-	`comments_id`	INT	NOT NULL,
-	`post_id`	INT	NOT NULL,
-	`content`	VARCHAR(255)	NOT NULL,
-	`author`	VARCHAR(10)	NOT NULL,
-	`created_at`	DATETIME(6)	NOT NULL
-);
-
-ALTER TABLE `MEMBER` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
-	`member_id`
-);
-
-ALTER TABLE `POST` ADD CONSTRAINT `PK_POST` PRIMARY KEY (
-	`post_id`,
-	`member_id`
-);
-
-ALTER TABLE `COMMENTS` ADD CONSTRAINT `PK_COMMENTS` PRIMARY KEY (
-	`comments_id`,
-	`post_id`
-);
-
-ALTER TABLE `POST` ADD CONSTRAINT `FK_MEMBER_TO_POST_1` FOREIGN KEY (
-	`member_id`
-)
-REFERENCES `MEMBER` (
-	`member_id`
-);
-
-ALTER TABLE `COMMENTS` ADD CONSTRAINT `FK_POST_TO_COMMENTS_1` FOREIGN KEY (
-	`post_id`
-)
-REFERENCES `POST` (
-	`post_id`
-);
+CREATE TABLE comments (
+                          comments_id int NOT NULL,
+                          post_id int NOT NULL,
+                          member_id int NOT NULL,
+                          content varchar(255) NOT NULL,
+                          created_at datetime(6) NOT NULL,
+                          PRIMARY KEY (comments_id),
+                          KEY cm_post_id_idx (post_id),
+                          KEY cm_member_id_idx (member_id),
+                          CONSTRAINT cm_member_id FOREIGN KEY (member_id) REFERENCES member (member_id),
+                          CONSTRAINT cm_post_id FOREIGN KEY (post_id) REFERENCES post (post_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='댓글'
+CREATE TABLE member (
+                        member_id int NOT NULL COMMENT '회원 ID',
+                        email varchar(25) NOT NULL COMMENT '이메일',
+                        password varchar(50) NOT NULL COMMENT '비밀번호',
+                        author varchar(10) NOT NULL COMMENT '이름',
+                        role varchar(30) NOT NULL COMMENT '권한',
+                        PRIMARY KEY (member_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='회원'
+CREATE TABLE post (
+                      post_id int NOT NULL COMMENT '게시글 ID',
+                      member_id int NOT NULL COMMENT '회원ID',
+                      title varchar(50) NOT NULL COMMENT '제목',
+                      content varchar(500) NOT NULL COMMENT '본문',
+                      author varchar(10) NOT NULL COMMENT '이름',
+                      create_at datetime(6) NOT NULL COMMENT '생성시간',
+                      PRIMARY KEY (post_id),
+                      KEY post_member_id_idx (member_id),
+                      CONSTRAINT post_member_id FOREIGN KEY (member_id) REFERENCES member (member_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='게시글'
