406aa362de3f36af30ddff09a5a7e843b9ea9942
Chaedonghyun <102035495+Chaedonghyun@users.noreply.github.com>
2024-04-18 14:20:26 +0900

    test
    test

--- src/main/java/com/firstpj/member/controller/MemberController.java
+++ src/main/java/com/firstpj/member/controller/MemberController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2024-04-16        hagjoon       최초 생성
+ * 2024-04-18
  */
 @RestController
 @RequestMapping("/api")
