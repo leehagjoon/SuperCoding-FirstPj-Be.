1131684eca70a46687fe78649150fd247d01cad5
keehan95 <colinbae95@gmail.com>
2024-04-19 14:57:22 +0900

    Delete NotFoundException.java

--- src/main/java/com/firstpj/member/service/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.firstpj.member.service.exceptions;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
