a8b447636775693222fb89467dba8c6048d29520
Chaedonghyun <102035495+Chaedonghyun@users.noreply.github.com>
2024-04-19 15:40:51 +0900

    PULL
    pull 작업

---
+++ src/main/java/com/firstpj/member/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.firstpj.member.service;
+
+import com.firstpj.member.model.CommentsRqModel;
+
+import java.util.List;
+
+public interface CommentService {
+    List<CommentsRqModel> findAllComments();
+}
